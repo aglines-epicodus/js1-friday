aed1deda37fd2ebc5b41f7748415d057697e43ea
Andrew Glines <aglines@gmail.com>
2017-06-30 12:08:26 -0700

    display name of doctor in browser successfully

--- js/doctor.js
+++ js/doctor.js
@@ -8,8 +8,9 @@ Doctor.prototype.getDoctor = function(medicalIssue) {
     console.log(result);
     var firstName = result.data[0].profile.first_name;
     var lastName = result.data[0].profile.last_name;
-    console.log("name = " + firstName + " " + lastName);
-    // get name from this object
+    var fullName = firstName + " " + lastName;
+    console.log("name = " + fullName);
+    $('.showDoctor').text("The name of your recommended doctor is " + fullName + ".");
   })
   .fail(function(error){
     console.log("fail");
@@ -18,5 +19,9 @@ Doctor.prototype.getDoctor = function(medicalIssue) {
 exports.getDoctorModule = Doctor;
 
 
+
+
+// Doc, Grumpy, Happy, Sleepy, Bashful, Sneezy, Dopey
+// delusions of grandeur, bipolar, manic, narcolepsy, social anxiety, common cold, fever?
 
 // curl "https://api.betterdoctor.com/2016-03-01/doctors?query='acne'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=96098a29a8a69869e94a335954688d76"
