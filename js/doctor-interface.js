83abdae0fbf6092c7daf39155ea00c6df61a5f17
Andrew Glines <aglines@gmail.com>
2017-06-30 11:55:43 -0700

    add js code to successfully parse JSON object for first and last name of returned doctor

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,14 @@
 var Doctor = require('./../js/doctor.js').getDoctorModule;
 
-var displayDoctor = function(malady, doctorData) {
-  $('.showDoctor').text("Your suggested doctor is " + doctorData + ".");
-}
+// var displayDoctor = function(malady, doctorData) {
+//   $('.showDoctor').text("Your suggested doctor is " + doctorData + ".");
+// }
 
 $(document).ready(function(){
   var newDoctorObject = new Doctor();
     // $("#formOne").submit(function(){
     //   var medicalIssue = $("input#choice1").val();
+    medicalIssue = 'acne';
     newDoctorObject.getDoctor(medicalIssue);
-  });
+  // });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -6,6 +6,10 @@ Doctor.prototype.getDoctor = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(result) {
     console.log(result);
+    var firstName = result.data[0].profile.first_name;
+    var lastName = result.data[0].profile.last_name;
+    console.log("name = " + firstName + " " + lastName);
+    // get name from this object
   })
   .fail(function(error){
     console.log("fail");
