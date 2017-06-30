08016862db60747b30b0a690f75be28b5ac0850a
Andrew Glines <aglines@gmail.com>
2017-06-30 14:36:34 -0700

    allow doctor.js to only capture complete list of first/lastnames from JSON object, and no more

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,8 +3,8 @@ var Doctor = require('./../js/doctor.js').getDoctorModule;
 $(document).ready(function(){
   var newDoctorObject = new Doctor();
     $("form#formOne").submit(function(){
-      var medicalIssue = $("#choice1").val();
       event.preventDefault();
+      var medicalIssue = $("#choice").val();
       console.log("medicalIssue is " + medicalIssue);
     // medicalIssue = 'acne';
     newDoctorObject.getDoctor(medicalIssue);
--- js/doctor.js
+++ js/doctor.js
@@ -3,24 +3,41 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {}
 
 Doctor.prototype.getDoctor = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=' + apiKey)
   .then(function(result) {
     console.log(result);
-    var firstName = result.data[0].profile.first_name;
-    var lastName = result.data[0].profile.last_name;
-    var fullName = firstName + " " + lastName;
-    // console.log("name = " + fullName);
-    // The following jquery replaces HTML with jquery text
-    $(".showDoctor").text("The name of your recommended doctor is " + fullName + ".");
+// TEST = STORE IN ARRAY
+  firstNames = [];
+  lastNames = [];
+  for (i=0 ; i<10 ; i++){
+    if ( result.data[i] !== undefined ) {
+      firstNames[i] = result.data[i].profile.first_name;
+      lastNames[i] = result.data[i].profile.last_name;
+      console.log(firstNames[i], lastNames[i]);
+    }
+  };
+
+
+
+
+// TESTING FOREACH
+    // $.each(result.data[0].profile, function(index, value) {
+    //   console.log(index + " : " + value); });
+    // $.each([11, 22], function( index, value) {
+    //     console.log(index + " : " + value); });
+
+// WORKING CODE:
+    // var firstName = result.data[0].profile.first_name;
+    // var lastName = result.data[0].profile.last_name;
+    // var fullName = firstName + " " + lastName;
+    // $(".showDoctor").text("The name of your recommended doctor is " + fullName + ".");
+
   })
   .fail(function(error){
     console.log("fail");
   });
 };
 exports.getDoctorModule = Doctor;
-
-
-
 
 // Doc, Grumpy, Happy, Sleepy, Bashful, Sneezy, Dopey
 // delusions of grandeur, bipolar, manic, narcolepsy, social anxiety, common cold, fever?
