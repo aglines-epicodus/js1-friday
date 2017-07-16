d89c716a33bcf78948d570d4737ffd405ce758c6
Andrew Glines <aglines@gmail.com>
2017-07-16 16:57:59 -0700

    move Jquery out of back end as recommended; update front end so user has access to doctor contact info

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,30 @@
 var Doctor = require('./../js/doctor.js').getDoctorModule;
 
+var displayDoctor = function(result) {
+  firstNames = [];
+  lastNames = [];
+  docPhone = [];
+  $(".showDoctor").empty();
+  $(".showDoctor").text("Here is a list of recommended doctors: ");
+  for (i=0 ; i<20 ; i++) {
+    if ( result.data[i] !== undefined ) {
+      firstNames[i] = result.data[i].profile.first_name;
+      lastNames[i] = result.data[i].profile.last_name;
+      // console.log(result.data[i]);
+      docPhone[i] = result.data[i].practices[0].phones[0].number;
+
+      $(".showDoctor").append("<p>" + firstNames[i] + " " + lastNames[i] + "<br><a href='tel:" + docPhone[i] + "'>" + docPhone[i] + "</a></p>");
+    }
+  }
+};
+
 $(document).ready(function(){
   var newDoctorObject = new Doctor();
     $("form#formOne").submit(function(){
       event.preventDefault();
       var medicalIssue = $("#choice").val();
-      console.log("medicalIssue is " + medicalIssue);
-    // medicalIssue = 'acne';
     newDoctorObject.getDoctor(medicalIssue);
   });
 });
+
+exports.displayDoctorFunction = displayDoctor;
--- js/doctor.js
+++ js/doctor.js
@@ -1,44 +1,15 @@
 var apiKey = require('./../.env').apiKey;
+var displayDoctor = require('./../js/doctor-interface.js').displayDoctorFunction;
 
 function Doctor() {}
 
 Doctor.prototype.getDoctor = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=' + apiKey)
   .then(function(result) {
-    console.log(result);
-// TEST = STORE IN ARRAY
-  firstNames = [];
-  lastNames = [];
-  $(".showDoctor").empty();
-  $(".showDoctor").text("Here is a list of recommended doctors: ");
-  for (i=0 ; i<20 ; i++) {
-    if ( result.data[i] !== undefined ) {
-      firstNames[i] = result.data[i].profile.first_name;
-      lastNames[i] = result.data[i].profile.last_name;
-      console.log(firstNames[i], lastNames[i]);
-
-      $(".showDoctor").append("<p>" + firstNames[i] + " " + lastNames[i] + "</p>");
-    }
-  };
-
-// TESTING FOREACH
-    // $.each(result.data[0].profile, function(index, value) {
-    //   console.log(index + " : " + value); });
-    // $.each([11, 22], function( index, value) {
-    //     console.log(index + " : " + value); });
-
-// WORKING CODE:
-    // var firstName = result.data[0].profile.first_name;
-    // var lastName = result.data[0].profile.last_name;
-    // var fullName = firstName + " " + lastName;
-    // $(".showDoctor").text("The name of your recommended doctor is " + fullName + ".");
-
-  })
+    displayDoctor(result);
+})
   .fail(function(error){
     console.log("fail");
   });
 };
 exports.getDoctorModule = Doctor;
-
-// Doc, Grumpy, Happy, Sleepy, Bashful, Sneezy, Dopey
-// delusions of grandeur, bipolar, manic, narcolepsy, social anxiety, common cold, fever?
